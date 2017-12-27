1def75574e1009b38be32dc1437c7cb1c7f880fc
Katy Carr <khcarr9@gmail.com>
2017-12-27 15:38:54 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,22 +6,22 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
-        :friend_1 => {:name => "Benvolio", :age => "17", :attitude => "worried"},
-        :friend_2 => {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
+        {:name => "Benvolio", :age => "17", :attitude => "worried"},
+        {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-        :friend_1 => {:name => "Steven", :age => "30", :attitude => "confused"},
-        :friend_2 => {:name => "Nurse", :age => "44", :attitude => "worried"}
+        {:name => "Steven", :age => "30", :attitude => "confused"},
+        {:name => "Nurse", :age => "44", :attitude => "worried"}
       ]
    }
 }
 
-  
+
 
 end
