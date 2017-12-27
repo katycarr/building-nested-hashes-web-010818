aafe788439b0512c6e299bd9ed44039836eabe41
Katy Carr <khcarr9@gmail.com>
2017-12-27 15:38:03 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,6 +6,8 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
+        :friend_1 => {:name => "Benvolio", :age => "17", :attitude => "worried"},
+        :friend_2 => {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
       ]
    }, 
    :capulet => {
@@ -14,6 +16,8 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        :friend_1 => {:name => "Steven", :age => "30", :attitude => "confused"},
+        :friend_2 => {:name => "Nurse", :age => "44", :attitude => "worried"}
       ]
    }
 }
